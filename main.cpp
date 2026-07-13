048ae36b104df2a2a3d0b580a09febdacf2d136c
ajayrathore4x <ajayx0094@gmail.com>
2026-07-13 19:49:59 +0530

    for a sorted array

--- main.cpp
+++ main.cpp
@@ -1,8 +1,27 @@
 #include<iostream>
+#include<vector>
 using namespace std;
 
+//check whether array with single elements is sorted or not ? ---||---
+bool issorted(vector<int>& arr){
+    if(arr.size()<2) return true;
+    if(arr[0]<arr[1]){
+    for(int i=0;i<arr.size()-1;i++){
+        if(arr[i]>arr[i+1]){
+            return false;
+        }
+    }
+ }else{
+    for(int i=0;i<arr.size()-1;i++){
+        if(arr[i]<arr[i+1]){
+            return false;
+        }
+    }
+ }
+ return true;
+}
 int main(){
-cout<<"hello world"<<endl;
-cout<<"hello world"<<endl;
+ vector<int>arr={5,4,3,2,1};
+ cout<<issorted(arr)<<endl;
     return 0;
 }
