c01c54882fe088aa31e13e9b723f8b95cc4d6a44
ajayrathore4x <ajayx0094@gmail.com>
2026-07-14 10:36:43 +0530

    book allocation

--- main.cpp
+++ main.cpp
@@ -2,26 +2,40 @@
 #include<vector>
 using namespace std;
 
-//check whether array with single elements is sorted or not ? ---||---
-bool issorted(vector<int>& arr){
-    if(arr.size()<2) return true;
-    if(arr[0]<arr[1]){
-    for(int i=0;i<arr.size()-1;i++){
-        if(arr[i]>arr[i+1]){
-            return false;
-        }
+// book allocation-----
+bool isvalid( const vector<int>& arr,int n,int m,int mid){
+    int student=1;int pages=0;
+    for(int i=0;i<n;i++){
+    if(arr[i]>mid) return false;
+    if(pages+arr[i]<=mid){
+        pages+=arr[i];
+    }else{
+        student++;
+        pages=arr[i];
     }
- }else{
-    for(int i=0;i<arr.size()-1;i++){
-        if(arr[i]<arr[i+1]){
-            return false;
-        }
     }
- }
- return true;
+    if(student<=m) return true;
+    else return false;
 }
-int main(){
- vector<int>arr={5,4,3,2,1};
- cout<<issorted(arr)<<endl;
-    return 0;
+int bookallocation(vector<int>& arr,int m){
+    if(m>arr.size()) return -1;
+    if(arr.empty()) return -1;
+    int n=arr.size();
+    long long sum=0;int startval=0;
+    for(int i=0;i<n;i++){
+        sum+=arr[i];
+        startval=max(startval,arr[i]);
+    }
+    int minvalue=sum;
+    int st=startval,end =sum;
+    while(st<=end){
+        int mid=st+((end-st)/2);
+        if(isvalid(arr,n,m,mid)){
+            minvalue=min(minvalue,mid);
+            end=mid-1;
+        }else{
+            st=mid+1;
+        }
+    }
+    return minvalue;
 }
