48646008a9b1f36da659a9a3a167ecaf747e1937
ajayrathore4x <ajayx0094@gmail.com>
2026-07-17 17:17:27 +0530

    better

--- main.cpp
+++ main.cpp
@@ -210,14 +210,14 @@ int compressstring(vector<char>& chars){
     return idx;
 }
 int primecount(int n){
-    vector<bool>isprime(n,true);
+    vector<bool>isprime(n+1,true);
         isprime[0] = isprime[1] = false;
     int count=0;
     for(int i=2;i<n;i++){
         if(isprime[i]){
             count++;
         if ((long long)i * i < n) {
-        for(int j=i*2;j<n;j=j+i){
+        for(int j=i*i;j<n;j=j+i){
             isprime[j]=false;
         }
        }
